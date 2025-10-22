fd046d9caccde6b13d53fc9311413028f6356e88
Carolina Sarto <csarto@ucsd.edu>
2025-10-22 13:06:18 -0700

    Add dummy function and tests

---
+++ MemAnalysis/dummy.py
@@ -0,0 +1,6 @@
+
+import numpy as np
+
+def dummy_function(x):
+    print(x)
+    return np.sqrt(x)
---
+++ tests/test_MemAnalysis.py
@@ -0,0 +1,8 @@
+
+
+import pytest
+import MemAnalysis as ma
+
+def test_dummy_function():
+    result = ma.dummy.dummy_function(16)
+    assert result == 4.0
