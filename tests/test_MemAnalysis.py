53a30d5854fc6cb9ae9783f86e7b2b48cae31a6a
Christopher T. Lee <ctlee@ucsd.edu>
2025-10-27 16:26:44 -0700

    Add test for system_report
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>

--- tests/test_MemAnalysis.py
+++ tests/test_MemAnalysis.py
@@ -2,7 +2,16 @@
 
 import pytest
 import MemAnalysis as ma
+import MDAnalysis as mda
+from MDAnalysisData import datasets
 
 def test_dummy_function():
     result = ma.dummy.dummy_function(16)
     assert result == 4.0
+
+
+def test_system_report():
+    data = datasets.fetch_membrane_peptide()
+    u = mda.Universe(data.topology, data.trajectory)
+    report = ma.util.system_report(u)
+    assert report['ALA'] == 9
