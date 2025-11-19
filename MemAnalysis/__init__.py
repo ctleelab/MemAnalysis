503ae3ba71738c5efad63b2cd171cfebd9ad678b
Carolina Sarto <csarto@ucsd.edu>
2025-11-18 18:14:25 -0800

    Add spectral analysis to init

--- MemAnalysis/__init__.py
+++ MemAnalysis/__init__.py
@@ -1,8 +1,28 @@
+#
+# MemAnalysis
+#
+# Copyright 2025- The MemAnalysis Authors
+# and the project initiators Carolina Sarto and Christopher T. Lee.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Please help us support development by citing the research
+# papers on the package. Check out https://github.com/ctleelab/MemAnalysis/
+# for more information.
 
+# Add imports here
+from .surface import (
+    normalized_grid,
+    derive_surface,
+    get_z_surface,
+    get_interpolated_z_surface,
+)
+from .base_spectral_analysis import MembraneSpectralAnalysis
 
 from . import dummy
-from . import util 
+from . import util
 from . import leaflet_analysis as la
 from . import area_per_lipid as apl
 from . import neighbor_analysis as na
-
